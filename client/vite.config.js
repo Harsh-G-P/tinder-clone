212b4956fccc0a71a96f27a8061ddf2b23eb8260
Harsh Prajapati <harsh@Mac.lan>
2025-07-07 20:34:34 +0530

    Project completed

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,23 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'vendor_react'
+            }
+            if (id.includes('some-big-lib')) {
+              return 'vendor_biglib'
+            }
+            return 'vendor'
+          }
+        }
+      }
+    }
+  },
   plugins: [
     react(),
     tailwindcss()
