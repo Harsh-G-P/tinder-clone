9fe7ff1358113df496e904b7fa23a03e306e8521
Harsh Prajapati <harsh@Mac.lan>
2025-08-21 21:42:27 +0530

    Tinder adding

--- api/server.js
+++ api/server.js
@@ -41,6 +41,14 @@ app.use('/api/users',userRoute)
 app.use('/api/matches',matchRoute)
 app.use('/api/messages',messageRoute)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,"/client/dist")))
+
+    app.get("/*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+    })
+}
+
 
 httpServer.listen(port,()=>{
     console.log('Server Started at this port 😸:' + ' ' + port)
--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.MODE === 'development' ? 'https://tinder-clone-backend-six.vercel.app/api' : 'https://tinder-clone-backend-six.vercel.app/api'
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:3000/api' : '/api'
 
 export const axiosInstance = axios.create({
     baseURL:BASE_URL,
